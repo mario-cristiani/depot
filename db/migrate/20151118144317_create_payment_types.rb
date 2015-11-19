cbe6ff94e9e19058f79fe772bac4a9422ec3b706
Viorel Tamas <tamasvio@gmail.com>
2015-11-19 09:29:45 +0200

    Populate payment types

--- db/migrate/20151118144317_create_payment_types.rb
+++ db/migrate/20151118144317_create_payment_types.rb
@@ -1,9 +1,17 @@
 class CreatePaymentTypes < ActiveRecord::Migration
-  def change
+  def up
     create_table :payment_types do |t|
       t.string :name
 
       t.timestamps null: false
     end
+
+    # Populate payent types
+    PaymentType.create([{name: 'Check'}, {name: 'Credit Card'}, {name: 'Purchase Order'}])
   end
+
+  def down
+    drop_table :payment_types
+  end
+
 end
